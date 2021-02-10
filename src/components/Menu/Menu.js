cca09edb3d3c41f4528f6cb1372a485f04b4bc65
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-02-10 11:33:58 +0530

    create Menu component with basic styling

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import plus from "../../images/plus.svg"
+import './Menu.css';
+
+function Menu() {
+	return (
+		<div className='Menu'>
+			<div className='SubMenu__1'>
+				<div className='bold Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<div className='italics Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<div className='link Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+			</div>
+			<div className="SubMenu__2">
+				<div className='h3 Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<div className='h4 Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<div className='blockquote Menu--item'>
+					<svg 
+						version="1.1" 
+						id="Capa_1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" 
+						style={{"enableBackground": "new 0 0 512 512"}} 
+						xmlSpace="preserve"
+						className="svg"
+					>
+						<g>
+							<g>
+								<path d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+			</div>
+			<div className="bottom__style">
+			</div>
+		</div>
+	)
+}
+
+export default Menu;
