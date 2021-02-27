cdcbce517a46d578484d0b1c441f1881035dec61
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-02-27 11:12:26 +0530

    add notes button

---
+++ src/components/NotesButton/NotesButton.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import SVGLogoContainer from '../SVGLogoContainer/SVGLogoContainer';
+
+import { notes } from '../../assets/inlineSVG/inlineSVG';
+
+import './NotesButton.css';
+
+class NotesButton extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isActive: false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		
+		this.setState({
+			isActive: !this.state.isActive
+		})
+	}
+	
+	render() {
+		return (
+			<button
+				className='NotesButton'
+				onClick={this.handleClick}
+			>
+				<SVGLogoContainer 
+					svg={notes.svgsrc}
+					isActive={this.state.isActive}
+					action='notes'
+					addClass={true}
+				/>
+			</button>
+		)
+	}
+}
+
+export default NotesButton;
