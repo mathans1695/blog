2e86945d1b8ce03bfa826c6825d811f34ba56993
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-02-27 09:23:34 +0530

    add h4 button

---
+++ src/components/HeadingButton4/HeadingButton4.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import SVGLogoContainer from '../SVGLogoContainer/SVGLogoContainer';
+
+import { heading_4 } from '../../assets/inlineSVG/inlineSVG';
+
+import './HeadingButton4.css';
+
+class HeadingButton4 extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isActive: false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		
+		this.setState({
+			isActive: !this.state.isActive
+		})
+	}
+	
+	render() {
+		return (
+			<button 
+				className='HeadingButton4'
+				onClick={this.handleClick}
+			>
+				<SVGLogoContainer 
+					svg={heading_4.svgsrc} 
+					isActive={this.state.isActive}
+					action='heading-four'
+				/>
+			</button>
+		)
+	}
+}
+
+export default HeadingButton4;
