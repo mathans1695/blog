950b7e0263225bcfc8e9afeb28124ebd44d9bc6f
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-02-27 09:23:22 +0530

    add h3 button

---
+++ src/components/HeadingButton3/HeadingButton3.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import SVGLogoContainer from '../SVGLogoContainer/SVGLogoContainer';
+
+import { heading_3 } from '../../assets/inlineSVG/inlineSVG';
+
+import './HeadingButton3.css';
+
+class HeadingButton3 extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isActive: false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		
+		this.setState({
+			isActive: !this.state.isActive
+		})
+	}
+	
+	render() {
+		return (
+			<button 
+				className='HeadingButton3'
+				onClick={this.handleClick}
+			>
+				<SVGLogoContainer 
+					svg={heading_3.svgsrc}
+					isActive={this.state.isActive}
+					action='heading-three'
+				/>
+			</button>
+		)
+	}
+}
+
+export default HeadingButton3;
